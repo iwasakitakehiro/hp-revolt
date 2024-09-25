58acb073982428b781baa8f7c59ee631b9db185d
iwasakiktakehiro <iwasakitakehiro2@gmail.com>
2024-09-25 15:17:41 +0900

    自動デプロイテスト

--- scripts/updateContent.js
+++ scripts/updateContent.js
@@ -1,15 +1,20 @@
-const fs = require("fs");
-const path = require("path");
+import { readFileSync, writeFileSync, existsSync } from "fs";
+import { join } from "path";
 
 const jsonFilePath = process.argv[2];
 
-if (!fs.existsSync(jsonFilePath)) {
+if (!existsSync(jsonFilePath)) {
   console.error(`JSON file not found: ${jsonFilePath}`);
   process.exit(1);
 }
 
-const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, "utf8"));
+const jsonData = JSON.parse(readFileSync(jsonFilePath, "utf8"));
 
 // JSONデータをAstroでインポートできるように、ファイルに書き出す
-const outputFilePath = path.join(__dirname, "../src/content/generated.json");
-fs.writeFileSync(outputFilePath, JSON.stringify(jsonData, null, 2), "utf8");
+const outputFilePath = join(
+  new URL(".", import.meta.url).pathname,
+  "../src/content/generated.json"
+);
+writeFileSync(outputFilePath, JSON.stringify(jsonData, null, 2), "utf8");
+
+console.log(`JSON data written to ${outputFilePath}`);
