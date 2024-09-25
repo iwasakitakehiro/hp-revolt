c02242a8b52c78120718a44d4b2753cb95d1c04e
iwasakiktakehiro <iwasakitakehiro2@gmail.com>
2024-09-25 15:39:45 +0900

    20240925作業分

--- scripts/updateContent.js
+++ scripts/updateContent.js
@@ -13,8 +13,6 @@ const jsonData = JSON.parse(readFileSync(jsonFilePath, "utf8"));
 // JSONデータをAstroでインポートできるように、ファイルに書き出す
 const outputFilePath = join(
   new URL(".", import.meta.url).pathname,
-  "../src/content/generated.json"
+  "../src/content/data/storeData.json"
 );
 writeFileSync(outputFilePath, JSON.stringify(jsonData, null, 2), "utf8");
-
-console.log(`JSON data written to ${outputFilePath}`);
