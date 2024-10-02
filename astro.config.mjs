4c3977eed7ce013593d5ff4eab65b7c2e07da110
iwasakiktakehiro <iwasakitakehiro2@gmail.com>
2024-10-02 14:49:33 +0900

    動的ページ生成テスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import json from "./src/content/data/storeData.json";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: `/rebolt/${json.path}`,
+  base: "/",
 });
