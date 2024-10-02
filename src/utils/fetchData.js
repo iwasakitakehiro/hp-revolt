5a63f64897bf025c5497abf4222b8075b0a34e06
iwasakiktakehiro <iwasakitakehiro2@gmail.com>
2024-10-02 12:51:45 +0900

    動的ページ生成テスト

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,21 @@
 export async function fetchData(url) {
-  const res = await getWorks(url);
-  return res;
-}
+  try {
+    const res = await fetch(url);
 
-// 製作実績を取得
-const getWorks = url => {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        resolve(data);
-      });
-  });
-};
+    // レスポンスのContent-Typeを確認
+    const contentType = res.headers.get("content-type");
+
+    if (contentType && contentType.includes("application/json")) {
+      // JSONとしてデータを返す
+      const data = await res.json();
+      return data;
+    } else {
+      // JSON以外のデータを受け取った場合、エラーメッセージを表示
+      const errorText = await res.text();
+      throw new Error(`Expected JSON, but received: ${errorText}`);
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
