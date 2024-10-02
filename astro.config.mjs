97daf7a131b3839fcc44f22ae14bcfd9c643d5c7
iwasakiktakehiro <iwasakitakehiro2@gmail.com>
2024-10-02 14:59:15 +0900

    動的ページ生成テスト

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import json from "./src/content/data/storeData.json";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  base: "/",
+  base: `/rebolt/kawasaki`,
 });
